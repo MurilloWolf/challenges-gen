d7d0c1bcfe22d1ffc4686e3d4249c68e17d529e0
MurilloWolf <mwolfcavalheiro@gmail.com>
2023-08-09 22:59:28 -0300

    feat: create 404 page

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,8 +1,24 @@
+import { Link } from "react-router-dom";
+import { CaretLeft } from "@phosphor-icons/react";
+import "./not-found.css";
+import NotFoundImg from "./assets/not-found.svg";
 export default function NotFound() {
   return (
     <>
-      <h1>404</h1>
-      <p>Not Found</p>
+      <div className="not-found--wrapper">
+        <div className="not-found--content">
+          <figure className="not-found--figure">
+            <img alt="Not Found" src={NotFoundImg} />
+          </figure>
+          <p className="not-found--paragraph">
+            Sorry we can't find this page 😢
+          </p>
+          <Link to="/" className="not-found--link">
+            <CaretLeft size={24} />
+            Go back to home
+          </Link>
+        </div>
+      </div>
     </>
   );
 }
