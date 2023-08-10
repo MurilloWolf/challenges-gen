7d20e2bb28d219c2be1e47e6629c302b370a3beb
MurilloWolf <mwolfcavalheiro@gmail.com>
2023-08-09 23:01:16 -0300

    fix: removing duplicate settings component

--- src/pages/RandomProject/index.tsx
+++ src/pages/RandomProject/index.tsx
@@ -1,10 +1,7 @@
-import { Settings } from "../../components";
-
 export default function RandomProject() {
   return (
     <div>
       <h1>Random Project</h1>
-      <Settings />
     </div>
   );
 }
